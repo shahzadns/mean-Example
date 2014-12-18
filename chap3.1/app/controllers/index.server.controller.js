43c99944082cc4183160a64e7b6b466b041bc9e1
Shahzad Soomro <shahzadcs@hotmail.co.uk>
2014-12-18 14:13:06 +0500

    Fixes "String is not a function" error.

--- chap3.1/app/controllers/index.server.controller.js
+++ chap3.1/app/controllers/index.server.controller.js
@@ -1,8 +1,7 @@
 exports.render = function(req, res){
-    res.send('Yay ! my express app works.');
+    //res.send('Yay ! my express app works.');
 
-    //TODO resolve the string is not a function
-    //res.render('index', {
-    //    title: 'testing'
-    //});
+    res.render('index', {
+        title: 'testing'
+    });
 };
